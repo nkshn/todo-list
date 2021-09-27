40a9ec942102f44f63aed7709042bbc1422b0ea1
nkshn <vovanika23413@gmail.com>
2021-09-27 17:24:05 +0300

    add render methon and getting todos from localStorage

--- index.js
+++ index.js
@@ -1,118 +1,91 @@
-let listOfItem = [];
+const KEY = "todo-tasks";
+
+let listOfItem = JSON.parse(window.localStorage.getItem(KEY)) || [];
 
 // selectors
 let createBtn = document.querySelector('#create');
 let taskInput = document.querySelector('#taskName');
 let taskList = document.querySelector('body > section > main > div.list');
 
+// template / render functions
+function template() {
+  if (listOfItem.length <= 0) {
+    return `<p>No tasks yet! Please add new one!</p>`
+  } else {
+    return listOfItem.map(item => {
+      let str = "";
+      str += `<div id=${item.id} class=${item.isDone === true ? 'list-item-done' : 'list-item'}>`;
+      str += `<input id=${item.id} type='checkbox' class='list-item-checkbox' ${item.isDone === true ? 'checked' : null}>`;
+      str += `<p id=${item.id} class='list-item-p'>${item.name}</p>`;
+      if(item.isDone === true) {
+        str += `<button id=${item.id} class='list-item-deleteBtn'>delete</button>`;
+      }
+      str += `</div>`;
+      return str;
+    }).join(''); // to delete comma from list
+  }
+}
+function render() {
+  saveLocalStorage();
+  taskList.innerHTML = template();
+}
+
 // event listeners
 createBtn.addEventListener('click', submitTaskBtnHandler);
-taskInput.addEventListener('input', function (event) {
-  console.log("user typing: ", event.target.value.length);
-  if (event.target.value.length >= 20) {
-    alert("max input val");
-  }
+taskInput.addEventListener('input', enteringTaskNameHander);
+document.addEventListener('keydown', keyboardHandler);
+taskList.addEventListener('click', taskClickHandler);
 
-  if (event.target.value.length === 0) {
-    createBtn.disabled = true;
-  } else {
-    createBtn.disabled = false;
-  }
-});
-document.addEventListener('keydown', function (event) {
-  // on user press enter button, we create task
-  if (event.keyCode === 13) {
-    submitTaskBtnHandler();
-  }
-});
-taskList.addEventListener('click', function (event) {
-  let gettedId = event.path[0].id;
-
-  switch (event.path[0].classList[0]) {
-    case "list-item-checkbox": // change status of task
-      console.log("pressed checkBox, id: ", gettedId);
-      changeStatusTaskById(gettedId); // change status item at data structure
-      changeStatusTaskElement(gettedId); // change status item at data dom
-      break;
-    case "list-item": // mark task as done
-      console.log("pressed div, id: ", gettedId);
-      changeStatusTaskById(gettedId); // change status item at data structure
-      changeStatusTaskElement(gettedId); // change status item at dom
-      break;
-    case "list-item-deleteBtn": // delete task
-      console.log("pressed delete, id: ", gettedId);
-      deleteTaskById(gettedId); // remove item from data structure
-      deleteTaskElement(gettedId); // remove item from dom
-      break;
-  }
-
-  console.log("list of tasks: ", listOfItem);
-});
-
-// handelers
+// handers
 function submitTaskBtnHandler() {
   let taskName = taskInput.value; // get task name from input field
   let generatedId = generateUniqueId(); // generate unique id for easy future manipulations
 
   // cheking if task name entered
   if (taskName.length !== 0) {
-    addNewTask(taskName, generatedId); // add task to data structure
+    addNewTask(taskName, generatedId);
 
-    taskList.appendChild(createTaskElement(taskName, generatedId)) // create element
+    render(); // render new ui
 
     taskInput.value = ""; // clear input element
 
     createBtn.disabled = true; // disable create task button
-
-    console.log("listOfItem: ", listOfItem);
   }
 }
-
-// DOM manipulations functions
-function createTaskElement(name, generateId) {
-  let taskItem = document.createElement('div');
-  let taskCheckbox = document.createElement('input');
-  let taskText = document.createElement('p');
-  let taskBtns = document.createElement('div');
-  let taskDeleteBtn = document.createElement('button');
-
-  taskCheckbox.id = generateId;
-  taskCheckbox.type = 'checkbox';
-  taskCheckbox.checked = false;
-
-  taskItem.id = generateId;
-  taskText.id = generateId;
-  taskDeleteBtn.id = generateId;
-
-  taskText.innerHTML = name; // set task name
-  taskDeleteBtn.innerHTML = 'delete'; // set task name
-
-  taskItem.classList.add('list-item'); // add styling to main task container
-  taskText.classList.add('list-item-p'); // add styling to main task container
-  taskCheckbox.classList.add('list-item-checkbox'); // add styling to main task container
-  taskDeleteBtn.classList.add('list-item-deleteBtn'); // add styling to main task container
-
-  taskItem.appendChild(taskCheckbox);
-  taskItem.appendChild(taskText);
-  taskItem.appendChild(taskBtns);
-
-  taskBtns.appendChild(taskDeleteBtn);
-
-  return taskItem;
+function keyboardHandler(event) {
+  if (event.keyCode === 13) { // on user press enter button, we create task
+    submitTaskBtnHandler()
+  }
 }
-function deleteTaskElement(id) {
-  document.getElementById(id).remove();
-}
-function changeStatusTaskElement(id) {
-  let checkboxElem = document.getElementById(id).querySelector('input[type=checkbox]');
-  let item = listOfItem.find(item => item.id === id);
-  checkboxElem.checked = item.isDone;
-
-  if(item.isDone === true) {
-    document.getElementById(id).classList.add('task-done');
+function enteringTaskNameHander(event) {
+  if (event.target.value.length === 0) {
+    createBtn.disabled = true;
   } else {
-    document.getElementById(id).classList.remove('task-done');
+    createBtn.disabled = false;
   }
+}
+function taskClickHandler(event) {
+  let gettedId = event.path[0].id;
+
+  switch (event.path[0].classList[0]) {
+    case "list-item-checkbox": // change status of task
+      console.log("pressed checkBox, id: ", gettedId);
+      changeStatusTaskById(gettedId); // change status item at data structure
+      render(); // re-render UI
+      break;
+    case "list-item": // mark task as done
+      console.log("pressed div, id: ", gettedId);
+      changeStatusTaskById(gettedId); // change status item at data structure
+      render(); // re-render UI
+      break;
+    case "list-item-deleteBtn": // delete task
+      console.log("pressed delete, id: ", gettedId);
+      deleteTaskById(gettedId); // remove item from data structure
+      render(); // re-render UI
+      break;
+  }
+
+  console.log("list of tasks: ", listOfItem);
 }
 
 // data manipulations functions
@@ -151,4 +124,9 @@ function generateUniqueId() {
   return Math.random().toString(16).slice(2);
 }
 
-// todo: add render function, where we can pass edited, sorted, filtered list of tasks
+// localStorage helpers
+function saveLocalStorage() {
+  window.localStorage.setItem(KEY, JSON.stringify(listOfItem));
+}
+
+render(); // render UI
